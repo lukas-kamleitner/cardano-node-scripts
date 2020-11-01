3a978a503214566ea4159ac3481e2f0609634968
Lukas Kamleitner <lukas.kamleitner@gmail.com>
2020-11-01 17:48:53 +0100

    Create restartRelayNodeX.sh

---
+++ relay/restartRelayNodeX.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Kill and restart node
+
+killall cardano-node
+sleep 3
+
+#kill tmux session
+tmux kill-session -t relay
+
+#restart node
+SCRIPT_PATH="/opt/cardano/cnode/scripts/startRelayNodeX.sh"
+
+sleep 1
+tmux new -s "relay" -d
+sleep 1
+tmux send-keys -t "relay" "$SCRIPT_PATH" C-m
