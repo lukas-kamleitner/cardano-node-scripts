df7bf238b7318c68d5145db50f94344173c2393b
Lukas Kamleitner <lukas.kamleitner@gmail.com>
2020-11-07 02:12:44 +0100

    Update calculate_leader_schedule.sh

--- producer/calculate_leader_schedule.sh
+++ producer/calculate_leader_schedule.sh
@@ -1,6 +1,6 @@
 cardano-cli shelley query ledger-state --mainnet --out-file /home/cardano-block/scripts/ledger/ledger.json
 
-sigmaValue=$(python3 /home/cardano-block/pooltool.io/leaderLogs/getSigma.py --ledger /home/cardano-block/scripts/ledger/ledger.json  --pool-id $(cat ${NODE_HOME}/stakepoolid.txt) | tail$
+sigmaValue=$(python3 /home/cardano-block/pooltool.io/leaderLogs/getSigma.py --ledger /home/cardano-block/scripts/ledger/ledger.json  --pool-id $(cat ${NODE_HOME}/stakepoolid.txt) | tail -n 1 | awk '{ print $2 }')
 
 echo ${sigmaValue}
 
