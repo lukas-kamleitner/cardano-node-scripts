85066c5e4dca14b5b6607e2fb7ca1d3122018741
Lukas Kamleitner <lukas.kamleitner@gmail.com>
2021-01-11 12:16:54 +0100

    Update calculate_leader_schedule.sh
    Update calculate leader schedule to node version v1.24.2

--- producer/calculate_leader_schedule.sh
+++ producer/calculate_leader_schedule.sh
@@ -1,8 +1,5 @@
-cardano-cli shelley query ledger-state --mainnet --out-file /home/cardano-block/scripts/ledger/ledger.json
+cardano-cli query ledger-state --mainnet --allegra-era  --out-file /opt/cardano/ledger/ledger.json
 
-sigmaValue=$(python3 /home/cardano-block/pooltool.io/leaderLogs/getSigma.py --ledger /home/cardano-block/scripts/ledger/ledger.json  --pool-id $(cat ${NODE_HOME}/stakepoolid.txt) | tail -n 1 | awk '{ print $2 }')
-
+sigmaValue=$(python3 /home/core/git/pooltool.io/leaderLogs/getSigma.py --ledger /opt/cardano/ledger/ledger.json  --pool-id $(cat /opt/cardano/cnode/priv/pool/KALU/stakepoolid.txt) | tail -n 1 | awk '{ print $2 }')
 echo ${sigmaValue}
-
-python3 /home/cardano-block/pooltool.io/leaderLogs/leaderLogs.py --pool-id $(cat ${NODE_HOME}/stakepoolid.txt) --sigma ${sigmaValue} --vrf-skey ${NODE_HOME}/vrf.skey --tz Europe/Vienna
-
+python3 /home/core/git/pooltool.io/leaderLogs/leaderLogs.py --pool-id $(cat /opt/cardano/cnode/priv/pool/KALU/stakepoolid.txt) --sigma ${sigmaValue} --vrf-skey /opt/cardano/cnode/priv/pool/KALU/vrf.skey --tz Europe/Vienna
